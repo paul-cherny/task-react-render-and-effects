95a488306ca86ac22567d562c99524ef7512701a
paul-cherny <paulinachernysh@gmail.com>
2025-05-21 18:11:27 +0300

    task done

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import { useState, useEffect, useCallback } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [lastMessage, setLastMessage] = useState(-1);
+    
+    const handleNewMessage = useCallback((message: number) => {
+        setLastMessage(message);
+    }, []);
+    
+    useEffect(() => {
+        subscribe(props.sourceId, handleNewMessage);
+        return () => unsubscribe(props.sourceId, handleNewMessage);
+    }, [props.sourceId, handleNewMessage]);
+    
+    useEffect(() => {
+        setLastMessage(-1);
+    }, [props.sourceId]);
+    
+    return <div>{props.sourceId}: {lastMessage}</div>;
 }
